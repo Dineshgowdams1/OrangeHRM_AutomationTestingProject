d6ddb2ca2ad0d2a0e1cf8846f3c14f3941b4060f
Dineshgowdams1 <131878752+Dineshgowdams1@users.noreply.github.com>
2026-04-13 10:19:30 +0530

    In branch a new commit

--- src/test/java/demo/Practice2.java
+++ src/test/java/demo/Practice2.java
@@ -5,6 +5,8 @@ public class Practice2 {
 	public static void main(String[] args) {
 
 		System.out.println("Git Repo updates");
+		System.out.println("An update practice");
+		System.out.println("In branch a new commit");
 
 	}
 
